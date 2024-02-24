317032dea9bb27fe6432800d1b86cced794bb309
Roberta Drayer <robertadrayer@gmail.com>
2024-02-24 11:57:14 -0800

    added unroll function and test

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,23 @@
 function unroll(squareArray) {
+    let result = [];
+    while (squareArray.length > 0) {
+        result = result.concat(squareArray.shift());
+        if (squareArray.length > 0 && squareArray[0].length > 0) {
+            for (let i = 0; i < squareArray.length; i++) {
+                result.push(squareArray[i].pop());
+            }
+        }
+        if (squareArray.length > 0) {
+            result = result.concat(squareArray.pop().reverse());
+        }
 
+        if (squareArray.length > 0 && squareArray[0].length > 0) {
+            for (let i = squareArray.length - 1; i >= 0; i--) {
+                result.push(squareArray[i].shift());
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -6,4 +6,14 @@ describe("#unroll", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it("correctly unrolls a 3X3 square array", function() {
+    const squareArray = [
+      [1,2,3],
+      [8,9,4],
+      [7,6,5]
+    ];
+    const expectedUnrolledArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    expect(unroll(squareArray)).toEqual(expectedUnrolledArray);
+  });
+
 });
